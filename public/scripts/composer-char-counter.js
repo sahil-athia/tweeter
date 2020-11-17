d3df1f7ea0427f319b59ff8ed176fb80fc78912b
Sahil Athia <sahilathia918@gmail.com>
2020-11-17 19:48:44 +0000

    use this for css selector and change text color to red when negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  $('.container output').text(140);
+  $(this).parent().find('.counter').text(140);
   $('#tweet-text').on('input', function(){
     let chars = 140
-    const inputLength = this.value.length
-    $('.container output').text(chars - inputLength);
+    const inputLength = chars - this.value.length
+
+    if (inputLength < 0) {
+      $(this).parent().find('.counter').css( "color", "red" );
+    } else {
+      $(this).parent().find('.counter').css( "color", "#545149" );
+    }
+    $(this).parent().find('.counter').text(inputLength);
+    // we are traversing from textarea up to form and then finding the output decendant.
   })
   
 });
