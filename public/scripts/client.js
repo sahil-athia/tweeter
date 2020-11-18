003e59bd428dd4339e9521ca825cc76a7513b09e
Sahil Athia <sahilathia918@gmail.com>
2020-11-18 18:45:58 +0000

    add an appending function to handle multiple tweets in a single database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
-  }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
   
   
   const createTweetElement = (data) => {
@@ -37,7 +50,7 @@ $(document).ready(function() {
         <i class="fas fa-heart"></i>
       </span>
     </footer>
-  </article>
+    </article>
       `
     return $tweet
   };
@@ -45,13 +58,10 @@ $(document).ready(function() {
 
   const renderTweets = (tweetData) => {
     for (const tweet of tweetData) {
-
+      $tweet = createTweetElement(tweet)
+      $('.container').append($tweet);
     }
   }
   
   renderTweets(data);
-  // const $tweet = createTweetElement(tweetData)
-  
-  // console.log($tweet)
-  // $('.container').append($tweet); 
 });
