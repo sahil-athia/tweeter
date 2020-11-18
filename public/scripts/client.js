2d2662c2c26b9c32803ca8eeb694206efb6f1351
Sahil Athia <sahilathia918@gmail.com>
2020-11-18 18:38:42 +0000

    add a function that appends a tweet data based on the article template

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,55 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
+  
+  
+  const createTweetElement = (data) => {
+    const date = new Date(data.created_at * 1000)
+    let $tweet = `
+    <article class="tweet">
+    <header>
+      <span>
+        <img src="${data.user.avatars}">
+        <i> ${data.user.name}</i>
+      </span>
+      <span class="user-handle"> <b>${data.user.handle}</b> </span>
+    </header>
+    <p id="tweet-line"><b>${data.content.text}</b></p>
+    <footer>
+      <span id="date">${date.toUTCString()}</span>
+      <span class="icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </span>
+    </footer>
+  </article>
+      `
+    return $tweet
+  };
 
+
+  const renderTweets = (tweetData) => {
+    for (const tweet of tweetData) {
+
+    }
+  }
+  
+  renderTweets(data);
+  // const $tweet = createTweetElement(tweetData)
+  
+  // console.log($tweet)
+  // $('.container').append($tweet); 
+});
